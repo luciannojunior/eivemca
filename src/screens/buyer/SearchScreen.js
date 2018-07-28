6dafbcd416a7980ff8969d32a3e567a2af3802cd
giuseppe <giuseppe.mongiovi@ccc.ufcg.edu.br>
2018-07-28 20:44:48 -0300

    searchbar and fake seller's list added to searchscreen

--- src/screens/buyer/SearchScreen.js
+++ src/screens/buyer/SearchScreen.js
@@ -1,22 +1,95 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TextInput } from "react-native";
+import { SearchBar, List, ListItem } from 'react-native-elements';
 
 const styles = StyleSheet.create({
+    textarea: {
+        height: 100,
+        width: "100%",
+        borderColor: 'gray',
+        borderWidth: 1,
+        fontSize: 14,
+        borderRadius: 4
+    },
+    inputText: {
+        height: 50,
+        width: "80%",
+        borderColor: 'gray',
+        fontSize: 14,
+        borderRadius: 4,
+        borderColor: 'black',
+        borderRadius: 10,
+        borderWidth: 1
+    },
     mainContent: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        marginTop: 70,
+        backgroundColor: '#fff'
     },
-    textSize: {
-        fontSize: 18
+    font16: {
+        fontSize: 18,
+        marginRight: 5,
+        marginTop: 10,
+        marginLeft: 20
+    },
+    inputContainer: {
+        width: "80%",
+        marginBottom: 15,
+        flexDirection: 'row'
     }
 });
 
+
+const sellers = [
+  {
+    name: 'Vendedor de pamonha'
+  },
+  {
+    name: 'Vendedor de cocada'
+  },
+  {
+    name: 'Vendedor de milho'
+  }
+]
+
 class SearchScreen extends React.Component {
+    static navigationOptions = {
+        header: null
+    };
+    
+    componentWillMount(){
+        this.state = { searchText: '' };
+    }
+
+    onClear() {
+        this.state.searchText = '';
+    }
+
+    onTextChanged(text) {
+        this.state.searchText = text;
+    }
+
     render() {
         return (
             <View style={styles.mainContent}>
-                <Text style={styles.textSize}>Search Screen</Text>
+                <SearchBar
+                  lightTheme
+                  showLoading
+                  clearIcon
+                  onChangeText={(text) => this.onTextChanged(text)}
+                  onClear={this.onClear()}
+                  placeholder='Procure por um ambulante...' />
+
+                <List>
+                  {
+                    sellers.map((item, i) => (
+                      <ListItem
+                        key={i}
+                        title={item.name}
+                      />
+                    ))
+                  }
+                </List>
             </View>
         );
     }
