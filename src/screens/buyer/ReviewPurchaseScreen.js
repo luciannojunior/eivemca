a4ff58337f899ccb218a0c0658e72133cd380e56
Giuseppe <giuseppe.mongiovi@ccc.ufcg.edu.br>
2018-07-29 19:13:44 -0300

    purchasescreen and reviewpurchasescreen built

--- src/screens/buyer/ReviewPurchaseScreen.js
+++ src/screens/buyer/ReviewPurchaseScreen.js
@@ -1,22 +1,91 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import { List, ListItem, Button, Input } from 'react-native-elements';
+import Header from "../../components/Header";
+import ProfileHeader from "../../components/ProfileHeader";
 
 const styles = StyleSheet.create({
     mainContent: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        marginTop: 10,
+        backgroundColor: '#fff'
     },
-    textSize: {
-        fontSize: 18
-    }
+    title: {
+        fontSize: 20,
+        marginLeft: 15
+    },
+    inputContainer: {
+        marginLeft: "3%",
+        marginBottom: 7,
+        flexDirection: 'row'
+    },
+    bottomView:{
+        width: '100%',
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 0
+    },
 });
+const products = [
+    {
+        name: 'Pamonha',
+        description: 'Bem quentinha',
+        price: 1.50,
+        amount: 2
+    },
+    {
+        name: 'Milho cozido',
+        description: 'Muito bom!',
+        price: 2.00,
+        amount: 3
+    },
+    {
+        name: 'Milho assado',
+        description: 'Na brasa',
+        price: 2.50,
+        amount: 1
+    }
+];
+
+const seller = {name: 'Seu chico'};
 
 class ReviewPurchaseScreen extends React.Component {
+    static navigationOptions = {
+        header: null
+    };
+
     render() {
         return (
             <View style={styles.mainContent}>
-                <Text style={styles.textSize}>Review Purchase Screen</Text>
+                <Header onPressBack={() => this.props.navigation.navigate('Home')} />
+                <ProfileHeader seller={seller} />
+                <List>
+                    {
+                        products.map((item, i) => (
+                            <ListItem
+                                leftIcon={{name: 'megaphone', color: 'green', type: 'entypo'}}
+                                key={i}
+                                title={item.name + ' (' + item.amount + ')'}
+                                textInput={true}
+                                textInputValue={'R$ ' + (item.amount*item.price)}
+                                textInputEditable={false}
+                                textInputStyle={{marginRight: 10}}
+                                subtitle={item.description}
+                                hideChevron
+                            />
+                        ))
+                    }
+                </List>
+                <View style={ styles.bottomView}>
+                    <Button
+                        textStyle={{color: 'black'}}
+                        transparent={true}
+                        title='Realizar compra'
+                        leftIcon={{name: 'megaphone', color: 'green', type: 'entypo'}}
+                    />
+                </View>
             </View>
         );
     }
