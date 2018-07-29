899d0bc4301df06fae8a474bf685e64fd957724b
Luciano Júnior <luciannojunior@gmail.com>
2018-07-29 19:12:22 -0300

    Firebase

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { RootNavigator } from './src/navigation/RootNavigator';
-import HomeScreen from './src/screens/HomeScreen';
+import * as firebase from 'firebase';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,6 +11,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+var config = {
+  apiKey: "AIzaSyAHACJD2UD8MYk1gm2zC4_F5NnVnql9QyI",
+  authDomain: "eivemca-1bc0b.firebaseapp.com",
+  databaseURL: "https://eivemca-1bc0b.firebaseio.com",
+  projectId: "eivemca-1bc0b",
+  storageBucket: "eivemca-1bc0b.appspot.com",
+  messagingSenderId: "168937478213"
+};
+
+firebase.initializeApp(config);
 
 export default class App extends React.Component {
   render() {
--- src/screens/seller/CreateProductScreen.js
+++ src/screens/seller/CreateProductScreen.js
@@ -1,95 +1,131 @@
 import React from "react";
-import { View, StyleSheet, Text, TextInput } from "react-native";
-
-import Header from '../../components/Header';
+import { Alert, View, StyleSheet, Text, TextInput } from "react-native";
+import Header from "../../components/Header";
+import * as firebase from "firebase";
 
 const styles = StyleSheet.create({
-    textarea: {
-        height: 100,
-        width: "100%",
-        borderColor: 'gray',
-        borderWidth: 1,
-        fontSize: 14,
-        borderRadius: 4,
-    },
-    inputText: {
-        height: 50,
-        width: "100%",
-        borderColor: 'gray',
-        fontSize: 14,
-        borderRadius: 4,
-    },
-    mainContent: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        backgroundColor: '#fff'
-    },
-    font16: {
-        fontSize: 16
-    },
-    inputContainer: {
-        marginBottom: 15
-    },
-    submitContainer: { 
-        alignItems: 'center',
-        marginTop: 40
-    }
+  textarea: {
+    height: 100,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    fontSize: 14,
+    borderRadius: 4
+  },
+  inputText: {
+    height: 50,
+    width: "100%",
+    borderColor: "gray",
+    fontSize: 14,
+    borderRadius: 4
+  },
+  mainContent: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "#fff"
+  },
+  font16: {
+    fontSize: 16
+  },
+  inputContainer: {
+    marginBottom: 15
+  },
+  submitContainer: {
+    alignItems: "center",
+    marginTop: 40
+  }
 });
 
 class CreateProductScreen extends React.Component {
-    static navigationOptions = {
-        header: null
-    };
-    
-    componentWillMount(){
-        this.state = { name: '', price: '', description: '' };
-    }
+  static navigationOptions = {
+    header: null
+  };
 
-    render() {
-        return (
-            <View style={styles.mainContent}>
-                <Header onPressBack={() => this.props.navigation.navigate('SellerMain')} />
-                <View style={{ width: "80%", justifyContent: 'center', marginTop: 20 }}>
-                    <View style={styles.inputContainer}>
-                        <Text style={styles.font16}>Nome do Produto</Text>
-                        <TextInput
-                            style={styles.inputText}
-                            onChangeText={(name) => this.setState({name})}
-                            value={this.state.name}
-                            underlineColorAndroid= 'rgba(0,0,0,1)'
-                        />
-                    </View>
-                    <View style={styles.inputContainer}>
-                        <Text style={styles.font16}>Preço</Text>
-                        <TextInput
-                            style={styles.inputText}
-                            onChangeText={(price) => this.setState({price})}
-                            value={this.state.price}
-                            underlineColorAndroid= 'rgba(0,0,0,1)'
-                        />
-                    </View>
-                    <View style={styles.inputContainer}>
-                        <Text style={styles.font16}>Descrição</Text>
-                        <TextInput
-                            placeholder = "Escreva aqui a descrição do produto."
-                            style = {styles.textarea}
-                            multiline = {true}
-                            autoGrow = {true}
-                            editable = {true}
-                            maxLength = {2000}
-                            underlineColorAndroid= 'rgba(0,0,0,0)'
-                            onChangeText={(description) => this.setState({description})}
-                            value={this.state.description}
-                        />
-                    </View>
-                    <View style={styles.submitContainer}>
-                        <Text style={{ fontSize: 20 }}>Cadastrar Produto</Text>
-                    </View>
-                </View>
-            </View>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.cadastrarProduto = this.cadastrarProduto.bind(this);
+  }
+  componentWillMount() {
+    this.setState({
+      name: "",
+      price: "",
+      description: "",
+      message: null,
+      enviando: false
+    });
+  }
+
+  async cadastrarProduto() {
+    this.setState({ enviando: true });
+    const { name: nome, price: preco, description: descricao } = this.state;
+    await firebase
+      .database()
+      .ref("vendedor")
+      .child("produtos")
+      .push({ nome, preco, descricao });
+    Alert.alert(
+      "Aviso",
+      "Produto Cadastrado com Sucesso",
+      [
+        {
+          text: "OK",
+          onPress: () => this.props.navigation.navigate("SellerMain")
+        }
+      ],
+      { cancelable: false }
+    );
+    this.setState({ enviando: false });
+  }
+
+  render() {
+    return (
+      <View style={styles.mainContent}>
+        <Header
+          onPressBack={() => this.props.navigation.navigate("SellerMain")}
+        />
+        <View style={{ width: "80%", justifyContent: "center", marginTop: 20 }}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.font16}>Nome do Produto</Text>
+            <TextInput
+              style={styles.inputText}
+              onChangeText={name => this.setState({ name })}
+              value={this.state.name}
+              underlineColorAndroid="rgba(0,0,0,1)"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.font16}>Preço</Text>
+            <TextInput
+              style={styles.inputText}
+              onChangeText={price => this.setState({ price })}
+              value={this.state.price}
+              underlineColorAndroid="rgba(0,0,0,1)"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.font16}>Descrição</Text>
+            <TextInput
+              placeholder="Escreva aqui a descrição do produto."
+              style={styles.textarea}
+              multiline={true}
+              autoGrow={true}
+              editable={true}
+              maxLength={2000}
+              underlineColorAndroid="rgba(0,0,0,0)"
+              onChangeText={description => this.setState({ description })}
+              value={this.state.description}
+            />
+          </View>
+          <View style={styles.submitContainer}>
+            <Text style={{ fontSize: 20 }} onPress={this.cadastrarProduto}>
+              Cadastrar Produto
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default CreateProductScreen;
