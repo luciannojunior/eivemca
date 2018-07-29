9b62bbb43b3058b5db710292098149030b2363c6
Luciano Júnior <luciannojunior@gmail.com>
2018-07-29 19:58:35 -0300

    Finalizando icones

--- src/screens/seller/SellerMainScreen.js
+++ src/screens/seller/SellerMainScreen.js
@@ -1,63 +1,99 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import { Icon } from "react-native-elements";
 
 import Header from "../../components/Header";
 import ProfileHeader from "../../components/ProfileHeader";
 
 const styles = StyleSheet.create({
-    mainContent: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center'
-    },
-    inputContainer: {
-        marginBottom: 20
-    },
-    fontSizeText: {
-      fontSize: 20
-    }
+  mainContent: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center"
+  },
+  inputContainer: {
+    marginBottom: 20,
+    flexDirection: 'row',
+    justifyContent: "flex-start",
+    alignItems: "center"
+  },
+  fontSizeText: {
+    fontSize: 20
+  }
 });
 
 class SellerMainScreen extends React.Component {
-    static navigationOptions = {
-        header: null
-    };
-    
-    render() {
-        const seller = {name: 'Rafael'};
+  static navigationOptions = {
+    header: null
+  };
 
-        return (
-            <View style={styles.mainContent}>
-                <Header onPressBack={() => this.props.navigation.navigate('Home')} />
-                <ProfileHeader seller={seller} />
-                <View style={{ width: "80%", justifyContent: 'center', marginTop: 20 }}>
-                    <View style={styles.inputContainer}>
-                        <Text
-                            onPress={() => this.props.navigation.navigate('CreateProduct')}
-                            style={styles.fontSizeText}>
-                            Cadastrar Produto
-                        </Text>
-                    </View>
-                    <View style={styles.inputContainer}>
-                        <Text style={styles.fontSizeText}>Pedidos e Rotas</Text>
-                    </View>
-                    <View style={styles.inputContainer}>
-                        <Text style={styles.fontSizeText}>Vendas Semanais</Text>
-                    </View>
-                    <View style={styles.inputContainer}>
-                        <Text
-                            onPress={() => this.props.navigation.navigate('ProductList')}
-                            style={styles.fontSizeText}>
-                            Produtos Cadastrados
-                        </Text>
-                    </View>
-                    <View style={styles.inputContainer}>
-                        <Text style={styles.fontSizeText}>Personalize sua Página</Text>
-                    </View>
-                </View>
-            </View>
-        );
-    }
+  render() {
+    const seller = { name: "Rafael" };
+
+    return (
+      <View style={styles.mainContent}>
+        <Header onPressBack={() => this.props.navigation.navigate("Home")} />
+        <ProfileHeader seller={seller} />
+        <View style={{ width: "80%", justifyContent: "center", marginTop: 20 }}>
+          <View style={styles.inputContainer}>
+            <Icon
+              style={{ marginRight: 5 }}
+              name="megaphone"
+              type="entypo"
+              color="green"
+            />
+            <Text
+              onPress={() => this.props.navigation.navigate("CreateProduct")}
+              style={styles.fontSizeText}
+            >
+              Cadastrar Produto
+            </Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <Icon
+              style={{ marginRight: 5 }}
+              name="megaphone"
+              type="entypo"
+              color="green"
+            />
+            <Text style={styles.fontSizeText}>Pedidos e Rotas</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <Icon
+              style={{ marginRight: 5 }}
+              name="megaphone"
+              type="entypo"
+              color="green"
+            />
+            <Text style={styles.fontSizeText}>Vendas Semanais</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <Icon
+              style={{ marginRight: 5 }}
+              name="megaphone"
+              type="entypo"
+              color="green"
+            />
+            <Text
+              onPress={() => this.props.navigation.navigate("ProductList")}
+              style={styles.fontSizeText}
+            >
+              Produtos Cadastrados
+            </Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <Icon
+              style={{ marginRight: 5 }}
+              name="megaphone"
+              type="entypo"
+              color="green"
+            />
+            <Text style={styles.fontSizeText}>Personalize sua Página</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default SellerMainScreen;
--- src/screens/seller/SellerProductListScreen.js
+++ src/screens/seller/SellerProductListScreen.js
@@ -1,85 +1,97 @@
 import React from "react";
 import { View, StyleSheet, Text, ScrollView } from "react-native";
 import * as firebase from "firebase";
+import { Icon } from "react-native-elements";
 
-import Header from '../../components/Header';
+import Header from "../../components/Header";
 
 const styles = StyleSheet.create({
-    mainContent: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-    },
-    mainTitle: {
-        fontSize: 28,
-        fontWeight: 'bold',
-        marginTop: 20
-    },
-    secondaryTitle: {
-        fontSize: 28,
-        fontWeight: 'bold'
-    },
-    padding10: {
-        padding: 10
-    },
-    row: {
-        flexDirection: 'row'
-    },
-    productList: {
-        paddingTop: 10,
-        paddingBottom: 10,
-        fontSize: 18
-    },
-    productPrice: {
-        paddingTop: 10,
-        paddingBottom: 10,
-        fontSize: 18,
-        fontWeight: 'bold'
-    }
+  mainContent: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#fff"
+  },
+  mainTitle: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 20
+  },
+  secondaryTitle: {
+    fontSize: 28,
+    fontWeight: "bold"
+  },
+  padding10: {
+    padding: 10
+  },
+  row: {
+    flexDirection: "row"
+  },
+  productList: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontSize: 18
+  },
+  productPrice: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontSize: 18,
+    fontWeight: "bold"
+  }
 });
 
 class SellerProductListScreen extends React.Component {
-    static navigationOptions = {
-        header: null
-    };
+  static navigationOptions = {
+    header: null
+  };
 
-    state = {
-        products: []
-    };
+  state = {
+    products: []
+  };
 
-    componentWillMount() {
-        const ref = firebase.database().ref('vendedor/produtos');
-        ref.on('value', (value) => {
-            this.setState({products: Object.values(value.val() || {})});
-        });
-    }
-    
-    render() {
-        const { products } = this.state;
+  componentWillMount() {
+    const ref = firebase.database().ref("vendedor/produtos");
+    ref.on("value", value => {
+      this.setState({ products: Object.values(value.val() || {}) });
+    });
+  }
 
-        return (
-            <View style={styles.mainContent}>
-                <Header onPressBack={() => this.props.navigation.navigate('SellerMain')} />
-                <Text style={styles.mainTitle}>Produtos</Text>
-                <Text style={styles.secondaryTitle}>Cadastrados</Text>
-                <View style={{ width: "80%"}}>
-                    <ScrollView>
-                        <View style={styles.padding10}>
-                            {products.map(product => {
-                                return (
-                                    <View style={styles.row}>
-                                        <Text style={styles.productList}>{product.nome}</Text>
-                                        <Text style={styles.productPrice}> - R$ {product.preco}</Text>
-                                    </View>
-                                );
-                            })}
-                        </View>
-                    </ScrollView>
-                </View>
+  render() {
+    const { products } = this.state;
+
+    return (
+      <View style={styles.mainContent}>
+        <Header
+          onPressBack={() => this.props.navigation.navigate("SellerMain")}
+        />
+        <Text style={styles.mainTitle}>Produtos</Text>
+        <Text style={styles.secondaryTitle}>Cadastrados</Text>
+        <View style={{ width: "80%" }}>
+          <ScrollView>
+            <View style={styles.padding10}>
+              {products.map(product => {
+                return (
+                  <View style={styles.row}>
+                    <Icon
+                      style={{ marginRight: 5 }}
+                      name="megaphone"
+                      type="entypo"
+                      color="green"
+                    />
+                    <Text style={styles.productList}>{product.nome}</Text>
+                    <Text style={styles.productPrice}>
+                      {" "}
+                      - R$ {product.preco}
+                    </Text>
+                  </View>
+                );
+              })}
             </View>
-        );
-    }
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default SellerProductListScreen;
