9e863c01ad5c5d739da100d4407640645180b8bc
Luciano Júnior <luciannojunior@gmail.com>
2018-07-30 00:16:05 -0300

    Finalizando

--- src/screens/seller/OrdersScreen.js
+++ src/screens/seller/OrdersScreen.js
@@ -61,32 +61,52 @@ class OrdersScreen extends React.Component {
         header: null
     };
 
+    componentWillMount() {
+        firebase.database().ref('pedidos').on('value', (snap) => {
+            const pedidos = Object.values(snap.val());
+            this.setState({pedidos});
+        });
+    }
+
     getTotalPrice(order) {
+        if (!order || !order.produtos) return 0;
         let total = 0;
         order.produtos.forEach((prod) => {
+            console.log('PRODUTO', prod);
             total += prod.subtotal;
         });
         return total;
     }
 
     getSubtitle(order){
-        let content = 'Endereço: ' + order.endereco + '. \nPedido: ';
-        for(let i = 0; i<order.produtos.length; i++) {
-            if(i==0) content += order.produtos[i].nome + ' (' + order.produtos[i].quantidade + ')';
-            else content += ', ' + order.produtos[i].nome + ' (' + order.produtos[i].quantidade + ')';
+        const formataPreco = (preco) => {
+            if (preco == 0){
+                preco = "000";
+            }
+            const reverted = ("" + preco).split("").reverse().join("");
+            return "R$ " + (reverted.substring(0,2) + "," + reverted.substring(2,reverted.length)).split("").reverse().join("");
         }
-        content += '.\nTotal: R$ ' + this.getTotalPrice(order) + '.';
+        if (!order || !order.produtos) return '--';
+        let content = 'Endereço: ' + order.endereco + '. \nPedido: ';
+        let arrayProdutos = Object.values(order.produtos);
+        for(let i = 0; i< arrayProdutos.length; i++) {
+            if(i==0) content += arrayProdutos[i].nome + ' (' + arrayProdutos[i].quantidade + ')';
+            else content += ', ' + arrayProdutos[i].nome + ' (' + arrayProdutos[i].quantidade + ')';
+        }
+        content += '.\nTotal: R$ ' + formataPreco(this.getTotalPrice(order)) + '.';
         return content;
     }
 
     render() {
+        if (!this.state || !this.state.pedidos) return null;
+        let { pedidos } = this.state;
         return (
             <KeyboardAvoidingView style={styles.mainContent} behavior={'padding'}>
                 <Header onPressBack={() => this.props.navigation.navigate("Home")} />
                 <ProfileHeader seller={seller} />
                 <ScrollView>
                     <List>
-                        {orders.map((item, i) => (
+                        {this.state.pedidos.map((item, i) => (
                             <ListItem
                                 leftIcon={{ name: "megaphone", color: "green", type: "entypo" }}
                                 key={i}
